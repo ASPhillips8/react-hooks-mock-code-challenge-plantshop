eaa3bd4c61cc28c4ee716b471a10756557bb90e3
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-06-21 15:43:05 -0400

    Display plants and change state of stock

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,10 +1,20 @@
-import React from "react";
-import PlantCard from "./PlantCard";
+import React, { useState, useEffect } from "react"
+import PlantCard from "./PlantCard"
 
 function PlantList() {
-  return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
-  );
+  const [plantsList, setPlantsList] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((response) => response.json())
+      .then((plantData) => setPlantsList(plantData))
+  }, [])
+
+  const displayPlants = plantsList.map((plant) => {
+    return <PlantCard key={plant.id} plant={plant} />
+  })
+
+  return <ul className="cards">{displayPlants}</ul>
 }
 
-export default PlantList;
+export default PlantList

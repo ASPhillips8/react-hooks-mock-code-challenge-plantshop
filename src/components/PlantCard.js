78f5f20ef08428dda0c4bad114c2a3185570809c
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-06-27 08:48:56 -0400

    Display plants and stock state

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState } from "react"
 
-function PlantCard() {
+function PlantCard({ plant }) {
+  const { id, name, image, price } = plant
+  const [isINStock, setIsInStock] = useState(true)
+
+  function handleStockClick() {
+    setIsInStock(!isINStock)
+  }
+
   return (
     <li className="card">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+      <p>Price: {price}</p>
+      {isINStock ? (
+        <button className="primary" onClick={handleStockClick}>
+          In Stock
+        </button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={handleStockClick}>Out of Stock</button>
       )}
     </li>
-  );
+  )
 }
 
-export default PlantCard;
+export default PlantCard
--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,10 +1,11 @@
-import React from "react";
-import PlantCard from "./PlantCard";
+import React from "react"
+import PlantCard from "./PlantCard"
 
-function PlantList() {
-  return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
-  );
+function PlantList({ plants }) {
+  const plantList = plants.map((plant) => {
+    return <PlantCard key={plant.id} plant={plant} />
+  })
+  return <ul className="cards">{plantList}</ul>
 }
 
-export default PlantList;
+export default PlantList
